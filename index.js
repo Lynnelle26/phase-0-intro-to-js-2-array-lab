952be900110d5023d7fc2566f1d7c8a6ffdc6337
lynnelle <lynnellewanja37@mail.com>
2024-03-21 22:26:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+  cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+  cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+function appendCat(){
+  const cats =["Milo", "Otis", "Garfield"]
+  const newCats = [...cats, "Broom"]
+ return newCats 
+}
+function prependCat(){
+  const cats =["Milo", "Otis", "Garfield"]
+  const newCats = ["Arnold", ...cats]
+  return newCats
+}
+function removeLastCat(){
+  const cats =["Milo", "Otis", "Garfield"]
+  const sliceCat =cats.slice(0,cats.length-1)
+  return sliceCat
+}
+function removeFirstCat(){
+  const cats =["Milo", "Otis", "Garfield"]
+  const removeCat = cats.slice(1)
+  return removeCat
+}
